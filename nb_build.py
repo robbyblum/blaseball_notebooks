f9bc7883a06100bc7734075352f7ac664ea0a5c1
Ryan Littleton <ryan.littleton0@gmail.com>
2021-03-22 19:24:10 -0400

    Add a redirect to the election vote totals

--- nb_build.py
+++ nb_build.py
@@ -101,6 +101,12 @@ for config_info in configs:
         print(f"Converted {file} -> {out_file[0].as_posix()}")
 
 
+# Generate redirect to Election Results Google Sheet
+with open(os.path.join(build_dir, 'election_results.html'), 'w', encoding='utf8') as fp:
+    fp.write("<head><meta http-equiv=\"refresh\" content=\"0;url=http://docs.google.com/spreadsheets/d/1v_1e2cxKoHvVejxXk4xHHC-YpPfKJXww15WIZ6nU7RU\"></head><body>Redirecting</body>")
+output_files.append((Path("election_results.html"), "Election Vote Totals"))
+print("Wrote election_results.html")
+
 HTML_start = """<!DOCTYPE html>
 <html>
 <body style="line-height:2;font-size:18px;font-weight:400;font-family:sans-serif;">
